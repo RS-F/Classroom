03bfa7f7731dd4a99f7d85e357c0e2b0f08f8124
RS-F <rsf.sea@gmail.com>
2019-11-22 08:34:57 +0100

    replace symbolic links to real files...

--- Classroom.java
+++ Classroom.java
@@ -1,1 +1,24 @@
-/home/ralf/JavaProjects/Classroom/src/Classroom.java
+
+public class Classroom {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Wilder wilder1 = new Wilder("Ralf",true);
+		
+		Wilder wilder2 = new Wilder("Steffen",true);
+		
+		Wilder wilder3 = new Wilder("Arne");
+		
+		Wilder wilder4 = new Wilder("");		
+		wilder4.setFirstname("Claus");
+		wilder4.setPresent(false);
+		
+		System.out.println(wilder1.whoAmI());
+		System.out.println(wilder2.whoAmI());
+		System.out.println(wilder3.whoAmI());
+		System.out.println(wilder4.whoAmI());
+		
+
+	}
+
+}
--- Wilder.java
+++ Wilder.java
@@ -1,1 +1,39 @@
-/home/ralf/JavaProjects/Classroom/src/Wilder.java
+
+public class Wilder {
+	private String firstname;
+	private boolean present;
+
+
+// Constructor
+public Wilder(String firstname) {
+    this.firstname = firstname;
+    this.present = false;
+}
+
+public Wilder(String firstname, boolean present) {
+    this.firstname = firstname;
+    this.present = present;
+}
+
+// Getter
+public String getFirstname() {
+    return this.firstname;
+}
+public boolean isPresent() {
+    return this.present;
+}
+
+// Setter
+public void setFirstname(String firstname) {
+    this.firstname = firstname;
+}
+public void setPresent(boolean present) {
+	this.present = present;
+}
+
+// WhoamI
+public String whoAmI() {
+	if (this.present) return ("My name is " + this.firstname + " and I am present");
+	else return ("My name is " + this.firstname + " and I'm not present");
+	}
+}
